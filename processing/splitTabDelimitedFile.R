69bbcc4768fb2f6d54f5bb318ff3cb849484ea08
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-12 10:36:24 +0100

    Added script for splitting tab-delimited file with a header line into smaller chunks of a given size with each retaining the header line.

---
+++ processing/splitTabDelimitedFile.R
@@ -0,0 +1,26 @@
+# Script for splitting a tab-delimited file into chunks each with a given number
+# of lines where the header line is retained in each
+
+# command line arguments
+args <- commandArgs(trailing=TRUE)
+
+if (length(args) != 4)
+  stop("Usage: splitTabDelimitedFile.R file prefix suffix lines")
+
+filename <- args[1]
+prefix <- args[2]
+suffix <- args[3]
+lines <- as.integer(args[4])
+
+library(readr)
+
+i <- 0
+
+writeFile <- function(data, pos)
+{
+	i <<- i + 1
+	write_tsv(data, paste(prefix, i, suffix, sep = "."))
+}
+
+read_tsv_chunked(filename, SideEffectChunkCallback$new(writeFile), chunk_size = lines)
+
