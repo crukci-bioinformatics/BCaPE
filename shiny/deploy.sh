1c3aad56322ae5f9a210e6b85d941e0bc50a6d08
Matthew Eldridge <eldrid01@nm156s016766.cri.camres.org>
2017-10-14 12:32:54 +0100

    Change permissions of data, analytics and log directory so that shiny user can access

--- shiny/deploy.sh
+++ shiny/deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run -u shiny -d --rm -p 80:3838 -v ${PWD}/data:/srv/shiny-server/bcape/data -v ${PWD}/analytics:/srv/shiny-server/bcape/analytics -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/bcape
+mkdir -p logs
+chmod ugo+rx data analytics
+chmod ugo+rwx logs
+docker run -u shiny -d --rm -p 80:3838 \
+  -v ${PWD}/data:/srv/shiny-server/bcape/data \
+  -v ${PWD}/analytics:/srv/shiny-server/bcape/analytics \
+  -v ${PWD}/logs:/var/log/shiny-server \
+  crukci-bioinformatics/bcape
