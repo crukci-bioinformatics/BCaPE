eb8a9d786bfd83ef07e34e01eb2100165be72309
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-26 16:23:47 +0100

    Changed port on which application is exposed and updated the image name to use the version on Docker Hub.

--- shiny/deploy.sh
+++ shiny/deploy.sh
@@ -2,8 +2,8 @@
 chmod ugo+r bcape.sqlite analytics.js
 mkdir -p logs
 chmod ugo+rwx logs
-docker run -u shiny -d --rm -p 80:3838 \
+docker run -u shiny -d -p 3838:3838 \
   -v ${PWD}/bcape.sqlite:/srv/shiny-server/bcape/bcape.sqlite \
   -v ${PWD}/analytics.js:/srv/shiny-server/bcape/analytics.js \
   -v ${PWD}/logs:/var/log/shiny-server \
-  crukci-bioinformatics/bcape
+  crukcibioinformatics/bcape
