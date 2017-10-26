678261a8bfc71dad15aa722546fce598e3fa01bb
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-26 16:32:56 +0100

    Removed scripts for building and deploying using Docker.

--- shiny/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chmod ugo+r bcape.sqlite analytics.js
-mkdir -p logs
-chmod ugo+rwx logs
-docker run -u shiny -d -p 3838:3838 \
-  -v ${PWD}/bcape.sqlite:/srv/shiny-server/bcape/bcape.sqlite \
-  -v ${PWD}/analytics.js:/srv/shiny-server/bcape/analytics.js \
-  -v ${PWD}/logs:/var/log/shiny-server \
-  crukcibioinformatics/bcape
