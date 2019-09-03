622534d1d23c86b7a0eb7aff5a66d29a363fe0d8
eldrid01 <matthew.d.eldridge@gmail.com>
2019-09-03 17:35:34 +0100

    Minor change to simplify code

--- processing/recalculate_copy_number_sensitivity_fdr.R
+++ processing/recalculate_copy_number_sensitivity_fdr.R
@@ -10,11 +10,6 @@ fdr <- signif(fdr, digits = 3)
 copyNumberGainSensitivity$fdr <- fdr[1:nrow(copyNumberGainSensitivity)]
 copyNumberLossSensitivity$fdr <- fdr[(nrow(copyNumberGainSensitivity) + 1):length(fdr)]
 
-copyNumberGainSensitivity %>%
-  mutate_at(vars(t.statistic, p.value, mean.gain, mean.nogain, fdr), signif, digits = 3) %>%
-  write_tsv("CopyNumberGainSensitivity.txt")
-
-copyNumberLossSensitivity %>%
-  mutate_at(vars(t.statistic, p.value, mean.loss, mean.noloss, fdr), signif, digits = 3) %>%
-  write_tsv("CopyNumberLossSensitivity.txt")
+write_tsv(copyNumberGainSensitivity, "CopyNumberGainSensitivity.adjusted.txt")
+write_tsv(copyNumberLossSensitivity, "CopyNumberLossSensitivity.adjusted.txt")
 
