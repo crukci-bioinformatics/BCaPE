dbb4dc9f7813a3810f28ada69c2784d3d5b17ed0
eldrid01 <matthew.d.eldridge@gmail.com>
2019-09-04 16:25:20 +0100

    Replace use of readTabDelimitedFile in missing functions.R script with tidyverse read_tsv function, specifying column types explicitly Use of read_tsv function

--- processing/create_sqlite_database.R
+++ processing/create_sqlite_database.R
@@ -1,49 +1,50 @@
-source("functions.R")
+library(tidyverse)
 
 # model classifications
-modelClassifications <- readTabDelimitedFile("ModelClassifications.txt", c("Model", "ER", "HER2", "PR", "BRCA1", "Type", "iC10", "3-Gene", "PAM50"), removeOtherColumns = TRUE)
+modelClassifications <- read_tsv("ModelClassifications.txt", col_types = cols(
+  Model = col_character(),
+  ER = col_character(),
+  HER2 = col_character(),
+  PR = col_character(),
+  BRCA1 = col_character(),
+  Type = col_character(),
+  iC10 = col_character(),
+  `3-Gene` = col_character(),
+  PAM50 = col_character()
+)) %>%
+  select(Model, ER, HER2, PR, BRCA1, Type, iC10, `3-Gene`, PAM50)
 
 # drug annotations
-drugAnnotations <- readTabDelimitedFile("DrugAnnotations.txt", c("Drug", "Pathway", "Target"), removeOtherColumns = TRUE)
+drugAnnotations <- read_tsv("DrugAnnotations.txt", col_types = cols(
+  Drug = col_character(),
+  Pathway = col_character(),
+  Target = col_character()
+)) %>%
+  select(Drug, Pathway, Target)
+
+# gene details
+geneDetails <- read_tsv("GeneDetails.txt", col_types = cols(
+  Symbol = col_character(),
+  Aliases = col_character(),
+  Name = col_character()
+)) %>%
+  select(Gene = Symbol, Aliases, Name)
 
 # drug sensitivity data
-drugSensitivity <- readTabDelimitedFile("DrugResponsesAUCModels.txt", c("Model", "Drug", "AUC"), removeOtherColumns = TRUE)
-
-drugSensitivity <- drugSensitivity %>%
+drugSensitivity <- read_tsv("DrugResponsesAUCModels.txt", col_types = cols(
+  Model = col_character(),
+  Drug = col_character(),
+  AUC = col_double()
+)) %>%
+  select(Model, Drug, AUC) %>%
+  filter(!is.na(AUC)) %>%
   mutate(AUC = signif(AUC, digits = 3))
 
-# copy number states
-copyNumberStates <- readTabDelimitedFile("CNAModels.txt", "Symbol")
-
-copyNumberStates <- copyNumberStates %>%
-  gather("Model", "State", -Symbol) %>%
-  filter(!is.na(State)) %>%
-  rename(Gene = Symbol) %>%
-  mutate(State = ifelse(State == "NEUT", "NEUTRAL", State)) %>%
-  mutate(State = tolower(State))
-
-# copy number sensitivity data
-copyNumberGainSensitivity <- readTabDelimitedFile("CopyNumberGainSensitivity.txt", c("Gene", "Drug", "t.statistic", "p.value", "mean.gain", "n.gain", "mean.nogain", "n.nogain", "fdr"))
-copyNumberLossSensitivity <- readTabDelimitedFile("CopyNumberLossSensitivity.txt", c("Gene", "Drug", "t.statistic", "p.value", "mean.loss", "n.loss", "mean.noloss", "n.noloss", "fdr"))
-
-p.values <- c(copyNumberGainSensitivity$p.value, copyNumberLossSensitivity$p.value)
-adjusted.p.values <- p.adjust(p.values, method = "BH")
-copyNumberGainSensitivity$fdr <- adjusted.p.values[1:nrow(copyNumberGainSensitivity)]
-copyNumberLossSensitivity$fdr <- adjusted.p.values[(nrow(copyNumberGainSensitivity) + 1):length(adjusted.p.values)]
-
-copyNumberGainSensitivity <- copyNumberGainSensitivity %>%
-  mutate_at(vars(t.statistic, p.value, mean.gain, mean.nogain, fdr), funs(signif(., digits = 3)))
-
-copyNumberLossSensitivity <- copyNumberLossSensitivity %>%
-  mutate_at(vars(t.statistic, p.value, mean.loss, mean.noloss, fdr), funs(signif(., digits = 3)))
-
 # mutation data
-mutations <- readTabDelimitedFile("SNVsModels.txt", "Symbol")
-
-mutations <- mutations %>%
-  gather(key = Model, value = Mutation, -Symbol) %>%
-  rename(Gene = Symbol) %>%
+mutations <- read_tsv("SNVsModels.txt") %>%
+  gather(key = Model, value = Mutation, -Gene) %>%
   mutate(Mutated = Mutation != "NO") %>%
+  select(Gene, Model, Mutated, Mutation) %>%
   separate_rows(Mutation, sep = "//") %>%
   mutate(Mutation = ifelse(Mutated, Mutation, NA)) %>%
   separate(Mutation, c("Chromosome", "Position", "Reference allele", "Alternate allele", "Type"), sep = "[:/_ ]", extra = "drop") %>%
@@ -52,63 +53,106 @@ mutations <- mutations %>%
   mutate(Type = ifelse(Type == "nonsynonymous", "non-synonymous", Type)) %>%
   mutate(Type = ifelse(Type == "nonframeshift", "non-frameshift", Type))
 
-# mutation sensitivity data
-mutationSensitivity <- readTabDelimitedFile("MutationSensitivity.txt", c("Gene", "Drug", "t.statistic", "p.value", "mean.mutation", "n.mutation", "mean.nomutation", "n.nomutation", "fdr"))
-
-mutationSensitivity <- mutationSensitivity %>%
-  mutate(fdr = p.adjust(p.value, method = "BH")) %>%
-  mutate_at(vars(t.statistic, p.value, mean.mutation, mean.nomutation, fdr), funs(signif(., digits = 3)))
-
 # expression data
-expression <- readTabDelimitedFile("ExpressionModels.txt", "Gene")
-
-expression <- expression %>%
+expression <- read_tsv("ExpressionModels.txt") %>%
   gather(key = Model, value = Expression, -Gene) %>%
   filter(!is.na(Expression)) %>%
   mutate(Expression = signif(Expression, digits = 3))
 
-# expression sensitivity data
-expressionSensitivity <- readTabDelimitedFile("ExpressionSensitivity.txt", c("Gene", "Drug", "p.value", "slope", "range.auc", "range.expression", "n", "fdr"))
-
-expressionSensitivity <- expressionSensitivity %>%
-  mutate(fdr = p.adjust(p.value, method = "BH")) %>%
-  mutate_at(vars(p.value, slope, range.auc, range.expression, fdr), funs(signif(., digits = 3)))
-
 # methylation data
-methylation <- readTabDelimitedFile("PromoterMethylationModels.txt", "Gene")
-
-methylation <- methylation %>%
+methylation <- read_tsv("PromoterMethylationModels.txt") %>%
   gather(key = Model, value = Methylation, -Gene) %>%
   filter(!is.na(Methylation)) %>%
   mutate(Methylation = signif(Methylation, digits = 3))
 
-# methylation sensitivity data
-methylationSensitivity <- readTabDelimitedFile("MethylationSensitivity.txt", c("Gene", "Drug", "p.value", "slope", "range.auc", "range.methylation", "n", "fdr"))
+# copy number states
+copyNumberStates <- read_tsv("CNAModels.txt") %>%
+  gather("Model", "State", -Gene) %>%
+  filter(!is.na(State)) %>%
+  mutate(State = ifelse(State == "NEUT", "NEUTRAL", State)) %>%
+  mutate(State = tolower(State))
 
-methylationSensitivity <- methylationSensitivity %>%
-  mutate(fdr = p.adjust(p.value, method = "BH")) %>%
-  mutate_at(vars(p.value, slope, range.auc, range.methylation, fdr), funs(signif(., digits = 3)))
+# sensitivity analysis results
+mutationSensitivity <- read_tsv("MutationSensitivity.txt", col_types = cols(
+  Gene = col_character(),
+  Drug = col_character(),
+  t.statistic = col_double(),
+  p.value = col_double(),
+  mean.mutation = col_double(),
+  n.mutation = col_integer(),
+  mean.nomutation = col_double(),
+  n.nomutation = col_integer(),
+  fdr = col_double()
+))
 
-# gene details
-geneDetails <- readTabDelimitedFile("GeneDetails.txt", c("Symbol", "Aliases", "Name"), removeOtherColumns = TRUE)
+expressionSensitivity <- read_tsv("ExpressionSensitivity.txt", col_types = cols(
+  Gene = col_character(),
+  Drug = col_character(),
+  p.value = col_double(),
+  slope = col_double(),
+  range.auc = col_double(),
+  range.expression = col_double(),
+  n = col_integer(),
+  fdr = col_double()
+))
+
+methylationSensitivity <- read_tsv("MethylationSensitivity.txt", col_types = cols(
+  Gene = col_character(),
+  Drug = col_character(),
+  p.value = col_double(),
+  slope = col_double(),
+  range.auc = col_double(),
+  range.methylation = col_double(),
+  n = col_integer(),
+  fdr = col_double()
+))
+
+copyNumberGainSensitivity <- read_tsv("CopyNumberGainSensitivity.adjusted.txt", col_types = cols(
+  Gene = col_character(),
+  Drug = col_character(),
+  t.statistic = col_double(),
+  p.value = col_double(),
+  mean.gain = col_double(),
+  n.gain = col_integer(),
+  mean.nogain = col_double(),
+  n.nogain = col_integer(),
+  fdr = col_double()
+))
+
+copyNumberLossSensitivity <- read_tsv("CopyNumberLossSensitivity.adjusted.txt", col_types = cols(
+  Gene = col_character(),
+  Drug = col_character(),
+  t.statistic = col_double(),
+  p.value = col_double(),
+  mean.loss = col_double(),
+  n.loss = col_integer(),
+  mean.noloss = col_double(),
+  n.noloss = col_integer(),
+  fdr = col_double()
+))
 
 # retain details of only the genes assessed by the various omics analyses
 genes <- bind_rows(
-  copyNumberStates %>% select(Gene),
   mutations %>% select(Gene),
   expression %>% select(Gene),
-  methylation %>% select(Gene)
+  methylation %>% select(Gene),
+  copyNumberStates %>% select(Gene)
 ) %>%
   distinct
 
 geneDetails <- geneDetails %>%
-  rename(Gene = Symbol) %>%
   right_join(genes, by = "Gene") %>%
-  arrange(Gene) %>%
-  mutate(Name = ifelse(is.na(Name), "", Name)) %>%
-  mutate(Aliases = ifelse(is.na(Aliases), "", Aliases)) %>%
-  mutate(Details = ifelse(Aliases == "", "", paste("[", Aliases, "]", sep = ""))) %>%
-  mutate(Details = ifelse(Details == "", Name, paste(Name, Details)))
+  arrange(Gene)
+
+# add display column to the gene details table
+geneDetails <- geneDetails %>%
+  mutate(
+    Details = str_trim(str_c(
+      str_replace_na(Name, ""),
+      ifelse(is.na(Aliases), "", str_c("[", Aliases, "]")),
+      sep = ifelse(is.na(Name), "", " ")
+    ))
+  )
 
 # add logical columns for availability of various data types
 geneDetails <- geneDetails %>%
@@ -121,7 +165,7 @@ geneDetails <- geneDetails %>%
   left_join(expressionSensitivity %>% select(Gene) %>% distinct %>% mutate(ExpressionSensitivity = TRUE), by = "Gene") %>%
   left_join(methylation %>% select(Gene) %>% distinct %>% mutate(Methylation = TRUE), by = "Gene") %>%
   left_join(methylationSensitivity %>% select(Gene) %>% distinct %>% mutate(MethylationSensitivity = TRUE), by = "Gene") %>%
-  mutate_if(is.logical, funs(!is.na(.)))
+  mutate_if(is.logical, replace_na, FALSE)
 
 # create sqlite3 database
 db <- src_sqlite("bcape.sqlite", create = TRUE)
